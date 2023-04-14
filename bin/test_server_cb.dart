dfabebcaf2feaa7df0bc0db494d6d107eeca668c
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2023-04-14 18:06:52 +0530

    Init

---
+++ bin/test_server_cb.dart
@@ -0,0 +1,5 @@
+import 'package:test_server_cb/test_server_cb.dart' as test_server_cb;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_server_cb.calculate()}!');
+}
---
+++ test/test_server_cb_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_server_cb/test_server_cb.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
